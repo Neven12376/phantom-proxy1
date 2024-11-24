4e07ab91ac31af72c47428c71adb09dd4118f571
DrNate1010 <158646132+DrNate1010@users.noreply.github.com>
2024-11-23 21:35:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 import { join } from "node:path";
 import { hostname } from "node:os";
 import wisp from "wisp-server-node";
-import rammerhead from '@rubynetwork/rammerhead';
-import { createRammerhead, shouldRouteRh, routeRhUpgrade, routeRhRequest } from '@rubynetwork/rammerhead';
-import { meteorPath } from 'meteorproxy'
+
 
 
 const __dirname = process.cwd();
@@ -20,15 +18,9 @@ app.use(express.static(publicPath));
 app.use("/uv/", express.static(uvPath));
 app.use("/epoxy/", express.static(epoxyPath));
 app.use("/baremux/", express.static(baremuxPath));
-app.use("/meteor/", express.static(meteorPath));
 
 const server = createServer();
-const rh = createRammerhead({
-    logLevel: 'debug', //Options are: disabled, debug, traffic, info, warn, error (default: debug)
-    reverseProxy: false, //whether or not this server is running behind a reverse proxy (default: false)
-    disableLocalStorageSync: false, //disable localstorage sync (not recommended) (default: false)
-    disableHttp2: false //disable http2 usage (default: false) (NOT RECOMMENDED)
-});
+
 
 app.use((req, res) => {
     res.status(404);
@@ -38,20 +30,12 @@ app.use((req, res) => {
 server.on("request", (req, res) => {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
     res.setHeader("Cross-Origin-Embedder-Policy", "anonymous");
-    if (shouldRouteRh(req)) {
-        routeRhRequest(rh /* from the createRammerhead function MUST be passed */, req, res)
-    }
-    else {
-        app(req, res);
-    }
+    app(req, res);
 });
 
 server.on("upgrade", (req, socket, head) => {
     if (req.url.endsWith("/wisp/")) {
         wisp.routeRequest(req, socket, head)
-    }
-    else if (rammerhead.shouldRouteRh(req)) {
-        rammerhead.routeRhUpgrade(rh, req, socket, head)
     }
     else {
         socket.end()
